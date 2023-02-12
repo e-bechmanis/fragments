171b64e08b65cd6a6b382bacb0976ea5c7879bb9
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-02-11 20:35:14 -0500

    deleted extra comments

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -6,6 +6,5 @@ const { createSuccessResponse } = require('../../../src/response');
  * Get a list of fragments for the current user
  */
 module.exports = (req, res) => {
-  // TODO: this is just a placeholder to get something working...
   res.status(200).json(createSuccessResponse({ fragments: [] }));
 };
