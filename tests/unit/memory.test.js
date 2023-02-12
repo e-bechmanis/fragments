7cadf1449ba9fe97656a2700f0c70c9455196422
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-02-11 22:57:13 -0500

    added unit tests for memory.js

--- tests/unit/memory.test.js
+++ tests/unit/memory.test.js
@@ -1,13 +1,32 @@
 const memory = require('../../src/model/data/memory');
 
-/* readFragment
-writeFragment
-readFragmentData,
-writeFragmentData */
+describe('memory read/write business logic check', () => {
+  test('writeFragment() should return void Promise', async () => {
+    const fragment = {
+      id: 'testId',
+      ownerId: 'testOwnerId',
+      created: '2021-11-02T15:09:50.403Z',
+      updated: '2021-11-02T15:09:50.403Z',
+      type: 'text/plain',
+      size: 256,
+    };
+    const result = await memory.writeFragment(fragment);
+    expect(result).toBe(undefined);
+  });
 
-describe('memory read/write check', () => {
-  test('should return Promise', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
+  test('writeFragmentData() should return void Promise', async () => {
+    const data = Buffer.from('test');
+    const result = await memory.writeFragmentData('testOwnerId', 'testId', data);
+    expect(result).toBe(undefined);
+  });
+
+  test('readFragment() should return Promise with fragment object', async () => {
+    const result = await memory.readFragment('testOwnerId', 'testId');
+    expect(result && typeof result === 'object').toBe(true);
+  });
+
+  test('readFragmentData() should return Promise', async () => {
+    const result = await memory.readFragment('testOwnerId', 'testId');
+    await expect(result && typeof result === 'object').toBe(true);
   });
 });
