ca14080e8dee9d2c82f9c0ce0f3c21380b3c59f6
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-02-20 22:28:56 -0500

    refactored, added headers to response

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -1,7 +1,7 @@
 // src/routes/api/getById.js
 
 const { Fragment } = require('../../model/fragment');
-const { createSuccessResponse, createErrorResponse } = require('../../response');
+const { createErrorResponse } = require('../../response');
 const logger = require('../../logger');
 /**
  * Gets an authenticated user's fragment data (i.e., raw binary data) with the given id
@@ -10,15 +10,22 @@ module.exports = async (req, res) => {
   try {
     logger.info('Trying to get user fragment by id // GET /v1/fragments/:id');
     const fragment = await Fragment.byId(req.user, req.params.id);
+    logger.debug('Read fragment metadata');
     logger.debug({ fragment });
     const fragmentData = await fragment.getData();
     const fragmentType = fragment.type;
+    const fragmentDataSize = fragment.size;
 
     logger.info('Fragment is successfully read');
+    logger.debug(`Fragment type ` + fragmentType);
+    logger.debug(`Fragment size ` + fragmentDataSize);
+
+    // set Content-type and Content-Length in response header
     return res
-      .set('Content-Type', fragmentType) // set Content-Type in header
+      .set('content-type', fragmentType)
+      .set('content-length', fragmentDataSize)
       .status(200)
-      .json(createSuccessResponse({ fragment: fragmentData }));
+      .send(fragmentData);
   } catch (err) {
     logger.error(err);
     return res.status(404).json(createErrorResponse(404, err));
