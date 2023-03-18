e631a76c6ad6b57a3823c57390ebab2293a44550
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-03-17 23:22:23 -0400

    refactored logic to account for extensions

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -9,16 +9,35 @@ const logger = require('../../logger');
 module.exports = async (req, res) => {
   try {
     logger.info('Trying to get user fragment by id // GET /v1/fragments/:id');
-    const fragment = await Fragment.byId(req.user, req.params.id);
-    logger.debug('Read fragment metadata');
-    logger.debug({ fragment });
+    //Logic to account for GET /fragments/:id.ext
+    //Parse id param to see if there is any extension to convert fragment to
+    const parsedId = req.params.id.split('.');
+    const id = parsedId[0]; // id will always be an element 0
+
+    // Getting fragment metadata
+    const fragment = await Fragment.byId(req.user, id);
+    logger.debug({ fragment }, `Read fragment metadata for user ${req.user}`);
+    // Getting fragment data
     const fragmentData = await fragment.getData();
     const fragmentType = fragment.type;
     const fragmentDataSize = fragment.size;
 
-    logger.info('Fragment is successfully read');
+    logger.info('Fragment data is successfully read');
     logger.debug(`Fragment type ` + fragmentType);
     logger.debug(`Fragment size ` + fragmentDataSize);
+
+    // When id string actually gets parsed into 2 elements, we should see if the fragment data can be converted
+    // into the required datatype
+    if (parsedId.length > 1) {
+      const ext = parsedId[1];
+      logger.info(
+        `User ${req.user} requested conversion of fragment from ${fragmentType} to ${ext}`
+      );
+      // If conversion is allowed
+      if (fragment.isValidConversion(ext)) {
+        logger.debug('Conversion is allowed');
+      }
+    }
 
     // set Content-type and Content-Length in response header
     return res
