c6d2239d0c2f4f2098236cb25f60e969c6ce3930
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-03-28 22:43:42 -0400

    refactored delete route

--- src/routes/api/delete.js
+++ src/routes/api/delete.js
@@ -8,17 +8,18 @@ module.exports = async (req, res) => {
   logger.info('Fragment deletion is triggered');
   try {
     const fragment = await Fragment.byId(req.user, req.params.id);
-    if (!fragment) {
+    logger.debug({ fragment }, `owner: ${req.user}`);
+    await Fragment.delete(req.user, req.params.id);
+    res.status(200).json(createSuccessResponse());
+  } catch (err) {
+    console.log(err);
+    if (err.message.startsWith('Could not locate')) {
       logger.warn(`Couldn't find a fragment ${req.params.id}. Aborting delete process`);
       return res
         .status(404)
         .json(createErrorResponse(404, `Could not locate fragment. Fragment id: ${req.params.id}`));
     }
-    logger.debug({ fragment }, `owner: ${req.user}`);
-    await Fragment.delete(req.user, req.params.id);
-    res.status(200).json(createSuccessResponse());
-  } catch (err) {
-    logger.error(`An error occured while trying to delete the fragment: ${err}`);
+    logger.error(`An error occurred while trying to delete the fragment: ${err}`);
     res.status(500).json(createErrorResponse(500, err.message));
   }
 };
