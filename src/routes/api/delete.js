211bf1fd17aa23fa4c61d7e185311a3f974ff0e1
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-04-12 21:01:23 -0400

    updated delete route

--- src/routes/api/delete.js
+++ src/routes/api/delete.js
@@ -12,13 +12,9 @@ module.exports = async (req, res) => {
     await Fragment.delete(req.user, req.params.id);
     res.status(200).json(createSuccessResponse());
   } catch (err) {
-    if (err.message.startsWith('Could not locate')) {
-      logger.warn(`Couldn't find a fragment ${req.params.id}. Aborting delete process`);
-      return res
-        .status(404)
-        .json(createErrorResponse(404, `Could not locate fragment. Fragment id: ${req.params.id}`));
-    }
-    logger.error(`An error occurred while trying to delete the fragment: ${err}`);
-    res.status(500).json(createErrorResponse(500, err.message));
+    logger.warn(`Couldn't find a fragment ${req.params.id}. Aborting delete process`);
+    return res
+      .status(404)
+      .json(createErrorResponse(404, `Could not locate fragment. Fragment id: ${req.params.id}`));
   }
 };
