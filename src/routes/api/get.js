6fc22540c57ef92d0d28b4ef5e64988bcecf7e8b
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-04-16 22:10:30 -0400

    updated get.js as per A2 feedback

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,6 +1,6 @@
 // src/routes/api/get.js
 
-const { createSuccessResponse } = require('../../../src/response');
+const { createSuccessResponse, createErrorResponse } = require('../../../src/response');
 const { Fragment } = require('../../model/fragment');
 const logger = require('../../logger');
 
@@ -11,15 +11,20 @@ module.exports = async (req, res) => {
   let fragments = [];
   // If there is a query in the get request, making sure to pass expand parameter as true
   // This way it will return metadata for each fragment
-  if (req.query.expand) {
-    fragments = await Fragment.byUser(req.user, true);
-    logger.info(`User ${req.user} requested detailed fragments list`);
-    logger.debug({ fragments });
-  } else {
-    // Otherwise returning a list of fragments ids
-    fragments = await Fragment.byUser(req.user);
-    logger.info(`User ${req.user} requested list of fragments ids`);
-    logger.debug({ fragments });
+  try {
+    if (req.query.expand) {
+      fragments = await Fragment.byUser(req.user, true);
+      logger.info(`User ${req.user} requested detailed fragments list`);
+      logger.debug({ fragments });
+    } else {
+      // Otherwise returning a list of fragments ids
+      fragments = await Fragment.byUser(req.user);
+      logger.info(`User ${req.user} requested list of fragments ids`);
+      logger.debug({ fragments });
+    }
+  } catch (err) {
+    logger.error(err, 'Got an error in GET /v1/fragments request');
+    return res.status(500).json(createErrorResponse(500, err));
   }
   res.status(200).json(createSuccessResponse({ fragments: fragments }));
 };
